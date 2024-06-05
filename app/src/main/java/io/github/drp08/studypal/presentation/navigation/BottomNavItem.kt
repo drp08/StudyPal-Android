a695b040c611ad230e56cfebadd99a649164080e
arabellahurrell <arabellahurrell@gmail.com>
2024-06-05 15:02:19 +0100

    MonthlyCalendarScreen added and functional

--- app/src/main/java/io/github/drp08/studypal/presentation/navigation/BottomNavItem.kt
+++ app/src/main/java/io/github/drp08/studypal/presentation/navigation/BottomNavItem.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import cafe.adriel.voyager.core.screen.Screen
 import io.github.drp08.studypal.presentation.screens.DailyCalendarScreen
 import io.github.drp08.studypal.presentation.screens.HomeScreen
+import io.github.drp08.studypal.presentation.screens.MonthlyCalendarScreen
 import io.github.drp08.studypal.presentation.screens.ProfileScreen
 
 sealed class BottomNavItem(
@@ -20,6 +21,6 @@ sealed class BottomNavItem(
     }
 
     data object Home : BottomNavItem("Home", Icons.Default.Home, HomeScreen)
-    data object Calendar : BottomNavItem("Calendar", Icons.Default.DateRange, DailyCalendarScreen)
+    data object Calendar : BottomNavItem("Calendar", Icons.Default.DateRange, MonthlyCalendarScreen)
     data object Profile : BottomNavItem("Profile", Icons.Default.Person, ProfileScreen)
 }

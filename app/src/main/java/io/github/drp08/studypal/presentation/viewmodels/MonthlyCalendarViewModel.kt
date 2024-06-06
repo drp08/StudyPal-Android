de6bec427b787eeb1d364604b956293bae04505e
arabellahurrell <arabellahurrell@gmail.com>
2024-06-06 14:36:48 +0100

    removed dummy classes

--- app/src/main/java/io/github/drp08/studypal/presentation/viewmodels/MonthlyCalendarViewModel.kt
+++ app/src/main/java/io/github/drp08/studypal/presentation/viewmodels/MonthlyCalendarViewModel.kt
@@ -2,6 +2,7 @@ package io.github.drp08.studypal.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import io.github.drp08.studypal.domain.entities.SessionEntity
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,12 +13,13 @@ import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
-class MonthlyCalendarView : ViewModel() {
+
+class MonthlyCalendarViewModel : ViewModel() {
     private val _currentDate = MutableStateFlow(Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date)
     val currentDate: StateFlow<LocalDate> = _currentDate.asStateFlow()
 
-    private val _events = MutableStateFlow<List<MonthlyEvent>>(emptyList())
-    val events: StateFlow<List<MonthlyEvent>> = _events.asStateFlow()
+    private val _events = MutableStateFlow<List<SessionEntity>>(emptyList())
+    val events: StateFlow<List<SessionEntity>> = _events.asStateFlow()
 
     fun setCurrentDate(date: LocalDate) {
         viewModelScope.launch {
@@ -25,21 +27,23 @@ class MonthlyCalendarView : ViewModel() {
         }
     }
 
-    fun addEvent(event: MonthlyEvent) {
+    fun addEvent(event: SessionEntity) {
         viewModelScope.launch {
             _events.value = _events.value.toMutableList().apply { add(event) }
         }
     }
 
-    fun getEventsForDate(date: LocalDate): List<MonthlyEvent> {
+    fun getEventsForDate(date: LocalDate): List<SessionEntity> {
         return _events.value.filter { event ->
-            event.startTime.date == date || event.endTime.date == date || (event.startTime.date < date && event.endTime.date > date)
+            val eventStartDate = LocalDateTime(
+                date.year, date.monthNumber, date.dayOfMonth,
+                event.startTime / 3600, (event.startTime % 3600) / 60
+            )
+            val eventEndDate = LocalDateTime(
+                date.year, date.monthNumber, date.dayOfMonth,
+                event.endTime / 3600, (event.endTime % 3600) / 60
+            )
+            eventStartDate.date == date || eventEndDate.date == date || (eventStartDate.date < date && eventEndDate.date > date)
         }
     }
 }
-
-data class MonthlyEvent(
-    val startTime: LocalDateTime,
-    val endTime: LocalDateTime,
-    val title: String,
-    val description: String? = null)

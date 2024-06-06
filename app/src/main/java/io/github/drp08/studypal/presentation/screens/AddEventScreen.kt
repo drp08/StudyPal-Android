7bfc35a1359ef74fd49ee445b22842eba818b7f9
Virginia Watson <vw722@ic.ac.uk>
2024-06-06 13:35:32 +0100

    feat: Added event name and date time to event screen

--- app/src/main/java/io/github/drp08/studypal/presentation/screens/AddEventScreen.kt
+++ app/src/main/java/io/github/drp08/studypal/presentation/screens/AddEventScreen.kt
@@ -2,12 +2,9 @@ package io.github.drp08.studypal.presentation.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,21 +13,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -44,10 +32,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
-import network.chaintech.ui.datepicker.WheelDatePickerView
+import network.chaintech.ui.datetimepicker.WheelDateTimePickerView
 import network.chaintech.utils.DateTimePickerView
+import network.chaintech.utils.TimeFormat
 import network.chaintech.utils.WheelPickerDefaults
-import kotlin.math.roundToInt
+import network.chaintech.utils.dateTimeToString
 
 object AddEventScreen : Screen {
     @Composable
@@ -59,7 +48,7 @@ object AddEventScreen : Screen {
         ) {
             Box(
                 modifier = Modifier
-                    .size(320.dp, 330.dp)
+                    .size(320.dp, 150.dp)
                     .border(
                         width = 1.dp,
                         color = Color.LightGray,
@@ -69,8 +58,7 @@ object AddEventScreen : Screen {
             ) {
                 Column {
                     EventNameTextField()
-                    ExamDateDialogueBox()
-                    StudyHoursDropDown()
+                    EventDateTimeDialogueBox()
                 }
             }
         }
@@ -78,7 +66,7 @@ object AddEventScreen : Screen {
 
     @Composable
     fun EventNameTextField() {
-        var subjectName by rememberSaveable { mutableStateOf("") }
+        var eventName by rememberSaveable { mutableStateOf("") }
 
         Box (
             modifier = Modifier
@@ -89,26 +77,25 @@ object AddEventScreen : Screen {
             contentAlignment = Alignment.Center,
         ) {
             OutlinedTextField(
-                value = subjectName,
-                onValueChange = { subjectName = it },
-                label = { Text(text = "Subject Name", color = Color.DarkGray) },
+                value = eventName,
+                onValueChange = { eventName = it },
+                label = { Text(text = "Event Name", color = Color.DarkGray) },
             )
         }
     }
 
-    // Todo: Change exam date to not include time
     @Composable
-    fun ExamDateDialogueBox() {
-        var showDatePicker by remember { mutableStateOf(false) }
-        var examDate by rememberSaveable { mutableStateOf("Exam Date (Optional)") }
+    fun EventDateTimeDialogueBox() {
+        var showDateTimePicker by remember { mutableStateOf(false) }
+        var eventDateTime by rememberSaveable { mutableStateOf("Event Date and Time") }
 
-        if (showDatePicker) {
-            WheelDatePickerView(
+        if (showDateTimePicker) {
+            WheelDateTimePickerView(
                 modifier = Modifier
                     .padding(top = 10.dp, bottom = 10.dp)
                     .fillMaxWidth(),
-                showDatePicker = showDatePicker,
-                title = "Add Exam Date",
+                showDatePicker = showDateTimePicker,
+                title = "Add Event Date & Time",
                 doneLabel = "Okay",
                 titleStyle = TextStyle(
                     fontSize = 18.sp,
@@ -120,6 +107,7 @@ object AddEventScreen : Screen {
                     fontWeight = FontWeight(600),
                     color = Color.Black
                 ),
+                timeFormat = TimeFormat.AM_PM,
                 yearsRange = IntRange(2024, 2300),
                 height = 180.dp,
                 rowCount = 5,
@@ -142,12 +130,13 @@ object AddEventScreen : Screen {
                 shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
                 dateTimePickerView = DateTimePickerView.DIALOG_VIEW,
                 onDoneClick = {
-                    examDate = ("Exam Date: ").plus(it.toString())
-                    showDatePicker = false },
+                    eventDateTime = ("Event Date Time: ")
+                        .plus(dateTimeToString(it, "hh:mm a dd-MM-yyyy"))
+                    showDateTimePicker = false },
                 selectorProperties = WheelPickerDefaults.selectorProperties(
                     borderColor = Color.DarkGray
                 ),
-                onDismiss = { showDatePicker = false }
+                onDismiss = { showDateTimePicker = false }
             )
         }
 
@@ -159,7 +148,7 @@ object AddEventScreen : Screen {
             contentAlignment = Alignment.CenterStart,
         ) {
             TextButton(
-                onClick = { showDatePicker = true },
+                onClick = { showDateTimePicker = true },
                 modifier = Modifier
                     .border(
                         width = 1.dp,
@@ -168,113 +157,9 @@ object AddEventScreen : Screen {
                     )
                     .padding()
             ) {
-                Text(text = examDate, color = Color.DarkGray)
+                Text(text = eventDateTime, color = Color.DarkGray)
             }
         }
     }
 
-
-    @Composable
-    fun StudyHoursDropDown() {
-        val studyHoursToChoose = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
-        val expanded = remember { mutableStateOf(false) }
-        var studyHours by rememberSaveable { mutableStateOf(studyHoursToChoose[0]) }
-
-        Row (
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier
-                .padding(start = 20.dp, end = 20.dp, bottom = 6.dp, top = 6.dp)
-                .background(Color.Transparent)
-                .fillMaxWidth(),
-        ) {
-            Text(
-                text = "Hours of study per week: ",
-            )
-            Box(
-                contentAlignment = Alignment.CenterStart,
-                modifier = Modifier
-                    .size(40.dp, 32.dp)
-                    .clip(RoundedCornerShape(2.dp))
-                    .border(
-                        width = 1.dp,
-                        color = Color.LightGray,
-                        shape = RoundedCornerShape(5.dp)
-                    )
-                    .clickable { expanded.value = !expanded.value }
-            ) {
-                Text(
-                    text = studyHours,
-                    modifier = Modifier.padding(start = 8.dp)
-                )
-                Icon (
-                    Icons.Default.ArrowDropDown,
-                    contentDescription = null,
-                    Modifier.align(Alignment.CenterEnd).fillMaxHeight()
-                )
-                DropdownMenu(
-                    expanded = expanded.value,
-                    onDismissRequest = { expanded.value = false }
-                ) {
-                    studyHoursToChoose.forEach { item ->
-                        DropdownMenuItem(
-                            text = { Text(text = item) },
-                            onClick = {
-                                studyHours = item
-                                expanded.value = false
-                            },
-                            modifier = Modifier.fillMaxSize()
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun ConfidenceSlider() {
-        var confidence by remember { mutableFloatStateOf(0f) }
-        Column {
-            Text(
-                text = "How confident are you with this",
-                modifier = Modifier
-                    .padding(start = 20.dp))
-            Text(
-                text = "subject?",
-                modifier = Modifier
-                    .padding(start = 20.dp))
-            Slider(
-                value = confidence,
-                onValueChange = { confidence = it.roundToInt().toFloat() },
-                modifier = Modifier.padding(start = 20.dp, end = 20.dp),
-                colors = SliderDefaults.colors(
-                    thumbColor = MaterialTheme.colorScheme.secondary,
-                    activeTrackColor = MaterialTheme.colorScheme.secondary,
-                    inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer
-                ),
-                steps = 10,
-                valueRange = 0f..10f
-            )
-            Row (
-                modifier = Modifier
-                    .padding(start = 35.dp, end = 35.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text (text = "Not", fontSize = 12.sp)
-                Text (text = "Very", fontSize = 12.sp)
-            }
-            Row (
-                modifier = Modifier
-                    .padding(start = 20.dp, end = 20.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text (text = "Confident", fontSize = 12.sp)
-                Text (text = "Confident", fontSize = 12.sp)
-            }
-        }
-    }
 }

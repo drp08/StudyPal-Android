7258656a84112e7a1b4b0c3c494be8623edb9464
Harini-Sritharar <harinisri2004@gmail.com>
2024-06-10 16:32:42 +0100

    feat: added shared prefs so user only needs to register once

--- app/src/main/java/io/github/drp08/studypal/MainActivity.kt
+++ app/src/main/java/io/github/drp08/studypal/MainActivity.kt
@@ -1,5 +1,7 @@
 package io.github.drp08.studypal
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,12 +11,26 @@ import io.github.drp08.studypal.presentation.App
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    companion object{
+        lateinit var prefs: SharedPreferences;
+        var firstTimeOpening: Boolean = true;
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        prefs = getSharedPreferences(getString(R.string.app_shared_prefs), Context.MODE_PRIVATE)
+        firstTimeOpening = prefs.getBoolean(getString(R.string.first_time_opening), true)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             App()
+        }
+        prefs?.let { sharedPreferences ->
+            if (firstTimeOpening) {
+                with (sharedPreferences.edit()) {
+                    putBoolean(getString(R.string.first_time_opening), false)
+                    apply()
+                }
+            }
         }
     }
 }
--- app/src/main/java/io/github/drp08/studypal/presentation/App.kt
+++ app/src/main/java/io/github/drp08/studypal/presentation/App.kt
@@ -1,13 +1,17 @@
 package io.github.drp08.studypal.presentation
 
+import android.provider.Settings.Global.getString
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.CurrentScreen
 import cafe.adriel.voyager.navigator.Navigator
+import io.github.drp08.studypal.MainActivity
+import io.github.drp08.studypal.R
 import io.github.drp08.studypal.presentation.navigation.BottomNavBar
 import io.github.drp08.studypal.presentation.navigation.BottomNavItem
 import io.github.drp08.studypal.presentation.screens.HomeScreen
@@ -16,8 +20,13 @@ import io.github.drp08.studypal.presentation.theme.StudyPalAndroidTheme
 
 @Composable
 fun App() {
+  val firstTime = MainActivity.firstTimeOpening
+    var screen: Screen = HomeScreen
+    if (firstTime){
+        screen = RegistrationScreen
+    }
     StudyPalAndroidTheme(darkTheme = false) {
-        Navigator(screen = RegistrationScreen) { navigator ->
+        Navigator(screen = screen) { navigator ->
             if (navigator.lastItem is HomeScreen) {
                 Navigator(BottomNavItem.Home.screen) { _ ->
                     Scaffold(

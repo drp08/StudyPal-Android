49c1cebdc19a17080e7da51c4f49050303f953e5
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-06-11 15:25:46 +0100

    feat: added friends routes

--- server/src/main/java/io/github/drp08/studypal/data/DbDAO.kt
+++ server/src/main/java/io/github/drp08/studypal/data/DbDAO.kt
@@ -1,6 +1,9 @@
 package io.github.drp08.studypal.data
 
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -26,9 +29,9 @@ fun addUser(name: String): Boolean {
     return transaction.insertedCount == 1
 }
 
-fun getUser(name: String): Pair<String, Int> {
+fun getUser(name: String): Pair<String, Int>? {
     return transaction {
-        User.selectAll().where { User.name eq name }.map { it[User.name] to it[User.xp] }.first()
+        User.selectAll().where { User.name eq name }.map { it[User.name] to it[User.xp] }.firstOrNull()
     }
 }
 
@@ -45,4 +48,28 @@ fun getFriends(name: String): List<String> {
     return transaction {
         Friends.selectAll().where { Friends.name eq name }.map { it[Friends.friendName] }
     }
+}
+
+fun addFriend(name: String, friendName: String): Boolean {
+    val transaction = transaction {
+        Friends.insert {
+            it[Friends.name] = name
+            it[Friends.friendName] = friendName
+        }
+
+        Friends.insert {
+            it[Friends.name] = friendName
+            it[Friends.friendName] = name
+        }
+    }
+
+    return transaction.insertedCount == 2
+}
+
+fun deleteFriend(name: String, friendName: String): Boolean {
+    val transaction = transaction {
+        Friends.deleteWhere { (Friends.name eq name) and (Friends.friendName eq friendName) }
+        Friends.deleteWhere { (Friends.name eq friendName) and (Friends.friendName eq name) }
+    }
+    return transaction == 2
 }
--- server/src/main/java/io/github/drp08/studypal/routes/DatabaseApi.kt
+++ server/src/main/java/io/github/drp08/studypal/routes/DatabaseApi.kt
@@ -1,6 +1,9 @@
 package io.github.drp08.studypal.routes
 
+import io.github.drp08.studypal.data.addFriend
 import io.github.drp08.studypal.data.addUser
+import io.github.drp08.studypal.data.deleteFriend
+import io.github.drp08.studypal.data.getFriends
 import io.github.drp08.studypal.data.getUser
 import io.github.drp08.studypal.data.updateXp
 import io.ktor.http.HttpStatusCode
@@ -8,6 +11,7 @@ import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
@@ -16,6 +20,7 @@ import io.ktor.server.routing.route
 fun Route.databaseApiRoutes() {
     route("/database") {
         userDbRoutes()
+        friendsDbRoutes()
     }
 }
 
@@ -23,20 +28,65 @@ fun Route.userDbRoutes() {
     route("/users") {
         get("/{name}") {
             val name = call.parameters["name"]!!
-            call.respond(getUser(name))
+            getUser(name)?.let {
+                call.respond(it)
+            } ?: call.respond(HttpStatusCode.NotFound, "$name user not found.")
         }
 
         post {
             val name = call.receive<String>()
             val success = addUser(name)
 
-            call.respond(if (success) HttpStatusCode.OK else HttpStatusCode.InternalServerError)
+            call.respond(
+                if (success) HttpStatusCode.OK else HttpStatusCode.InternalServerError
+            )
         }
 
         put {
             val body = call.receive<String>()
             val (name, xp) = body.split(" ").run { this[0] to this[1].toInt() }
             updateXp(name, xp)
+        }
+    }
+}
+
+fun Route.friendsDbRoutes() {
+    route("/friends") {
+        get("/{name}") {
+            val name = call.parameters["name"]!!
+            call.respond(getFriends(name))
+        }
+
+        post {
+            val body = call.receive<String>()
+            val (name, friend) = body.split(" ").run { this[0] to this[1] }
+
+            getUser(name) ?:
+                return@post call.respond(HttpStatusCode.NotFound, "$name user not found.")
+            getUser(friend) ?:
+                return@post call.respond(HttpStatusCode.NotFound, "$friend user not found.")
+
+            val success = addFriend(name, friend)
+
+            call.respond(
+                if (success) HttpStatusCode.OK else HttpStatusCode.InternalServerError
+            )
+        }
+
+        delete {
+            val body = call.receive<String>()
+            val (name, friend) = body.split(" ").run { this[0] to this[1] }
+
+            getUser(name) ?:
+                return@delete call.respond(HttpStatusCode.NotFound, "$name user not found.")
+            getUser(friend) ?:
+                return@delete call.respond(HttpStatusCode.NotFound, "$friend user not found.")
+
+            val success = deleteFriend(name, friend)
+
+            call.respond(
+                if (success) HttpStatusCode.OK else HttpStatusCode.InternalServerError
+            )
         }
     }
 }

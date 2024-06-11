5a4c89d84b9fc193cd42ee06738d5c64ca2325d5
Virginia-Watson <114754547+Virginia-Watson@users.noreply.github.com>
2024-06-11 18:17:42 +0100

    feat: new profile screen (#35)
    * feat: adding stuff from other branch
    
    * feat: adding existing subjects ui
    
    * feat: adding expand topics to existing subjects
    
    * feat: adding statistics
    
    * feat: adding comments for viewModel and back-end clarity
    
    * fix: correct dependency format
    
    ---------
    
    Co-authored-by: Virginia Watson <vw722@ic.ac.uk>

--- app/src/main/java/io/github/drp08/studypal/presentation/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/io/github/drp08/studypal/presentation/viewmodels/ProfileViewModel.kt
@@ -4,19 +4,30 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.drp08.studypal.db.daos.SubjectDao
+import io.github.drp08.studypal.db.daos.TopicDao
 import io.github.drp08.studypal.db.session.UserSession
+import io.github.drp08.studypal.domain.entities.SessionEntity
+import io.github.drp08.studypal.domain.entities.SubjectEntity
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val userSession: UserSession
+    private val userSession: UserSession,
+    private val topicDao: TopicDao,
+    private val subjectDao: SubjectDao
 ): ViewModel() {
     companion object {
         private const val TAG = "ProfileViewModel"
     }
 
+    private val _subjects = MutableStateFlow<List<SubjectEntity>>(emptyList())
+    val subjects: StateFlow<List<SubjectEntity>> = _subjects.asStateFlow()
 
     init {
         viewModelScope.launch {
@@ -25,4 +36,8 @@ class ProfileViewModel @Inject constructor(
             }
         }
     }
+
+//    fun getAllSubjects() : List<SubjectEntity> {
+//        return subjectDao.getAllSubjects()
+//    }
 }

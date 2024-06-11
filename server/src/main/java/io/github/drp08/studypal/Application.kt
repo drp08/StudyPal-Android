bfdee66bf7b42bab163d080ceb4f395fe42be8f8
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-06-11 17:08:14 +0100

    docs: added swagger documentation file

--- server/src/main/java/io/github/drp08/studypal/Application.kt
+++ server/src/main/java/io/github/drp08/studypal/Application.kt
@@ -3,6 +3,7 @@ package io.github.drp08.studypal
 import io.github.drp08.studypal.routes.databaseApiRoutes
 import io.github.drp08.studypal.routes.scheduleRouting
 import io.github.drp08.studypal.utils.HttpConstants
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
@@ -10,6 +11,9 @@ import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.cors.routing.CORS
+import io.ktor.server.plugins.openapi.openAPI
+import io.ktor.server.plugins.swagger.swaggerUI
 import io.ktor.server.resources.Resources
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
@@ -28,8 +32,16 @@ fun main() {
 fun Application.module() {
     configureSerialization()
     configureResources()
+    configureCors()
     configureRouting()
     configureDatabase()
+}
+
+private fun Application.configureCors() {
+    install(CORS) {
+        anyHost()
+        allowHeader(HttpHeaders.ContentType)
+    }
 }
 
 fun Application.configureDatabase() {
@@ -45,6 +57,7 @@ private fun Application.configureResources() {
 
 fun Application.configureRouting() {
     routing {
+        openAPI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
         scheduleRouting()
         databaseApiRoutes()
 

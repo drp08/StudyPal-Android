db2cc77c5c4a6e8d77a2e0531de0c22d0a75e26b
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-06-11 13:12:33 +0100

    fix: each subject time does not exceed their max

--- server/src/main/java/io/github/drp08/studypal/scheduler/RandomiseScheduler.kt
+++ server/src/main/java/io/github/drp08/studypal/scheduler/RandomiseScheduler.kt
@@ -21,9 +21,6 @@ class RandomiseScheduler : Scheduler {
         fixedSessions: List<Session>,
         user: User
     ): List<Session> {
-        if (subjects.isEmpty())
-            return emptyList()
-
         val sessions: MutableList<Session> = mutableListOf<Session>().apply {
             addAll(fixedSessions)
         }
@@ -33,11 +30,15 @@ class RandomiseScheduler : Scheduler {
 
         // to ensure that a time isn't 'double-booked'
         val scheduledHours = mutableListOf<LongRange>()
+        val subjectTotalTime = mutableMapOf<String, Long>()
 
         var trials = 0
-        outer@for (studyHours in 0..user.maxStudyingHours) {
+        outer@ for (studyHours in 0..user.maxStudyingHours) {
             // chooses a random subject
-            val subject: Subject = subjects.random()
+            val subject: Subject = subjects
+                .filter { subjectTotalTime.getOrDefault(it.name, 0) < it.hoursPerWeek * HOUR_IN_MILLIS }
+                .also { if (it.isEmpty()) return emptyList() }
+                .random()
 
             val subjectTopics: List<Topic> = topics.filter { it.subject == subject.name }
             if (subjectTopics.isEmpty()) // Make sure the subject have topics.
@@ -70,6 +71,8 @@ class RandomiseScheduler : Scheduler {
                 )
 
             sessions.add(session)
+            subjectTotalTime[subject.name] =
+                subjectTotalTime.getOrDefault(subject.name, 0) + SESSION_DURATION
         }
         return sessions
             .apply { removeAll(fixedSessions) }

f69ccb2b6215189ed517c33527f0ac979e141289
arabellahurrell <arabellahurrell@gmail.com>
2024-06-13 09:57:49 +0100

    feat: changed colour of button

--- app/src/main/java/io/github/drp08/studypal/presentation/screens/PomodoroScreen.kt
+++ app/src/main/java/io/github/drp08/studypal/presentation/screens/PomodoroScreen.kt
@@ -11,15 +11,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -33,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
@@ -190,7 +187,7 @@ data class PomodoroScreen(
                 }
             },
             dismissButton = {
-                Button(onClick = onCancel) {
+                Button(onClick = onCancel, colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
                     Text(text = "Cancel")
                 }
             }

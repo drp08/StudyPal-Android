0492619074179ab3db9875d0bda79586ca07cd2a
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-06-16 20:21:58 +0100

    debug: added log statements to understand behaviour [skip ci]

--- app/src/main/java/io/github/drp08/studypal/data/v2/LocalSchedulingRepository.kt
+++ app/src/main/java/io/github/drp08/studypal/data/v2/LocalSchedulingRepository.kt
@@ -1,5 +1,6 @@
 package io.github.drp08.studypal.data.v2
 
+import android.util.Log
 import io.github.drp08.studypal.db.daos.SessionDao
 import io.github.drp08.studypal.db.daos.SubjectDao
 import io.github.drp08.studypal.db.daos.TopicDao
@@ -19,14 +20,22 @@ class LocalSchedulingRepository @Inject constructor(
     private val userRepository: UserRepository,
     private val scheduler: Scheduler
 ): SchedulingRepository {
+
+    companion object {
+        private const val TAG = "LocalSchedulingReposito"
+    }
+
     override suspend fun rescheduleAllSessions() = flow {
+        Log.d(TAG, "rescheduleAllSessions() called")
         val subjects = subjectDao.getAllSubjects()
         val topics = topicDao.getAllTopics()
         val user = userRepository.getUser()
 
         withContext(Dispatchers.IO) {
             val newSessions = scheduler.schedule(subjects, topics, emptyList(), user)
+            Log.d(TAG, "rescheduleAllSessions: returned newSessions = $newSessions")
             newSessions.forEach {
+                Log.d(TAG, "rescheduleAllSessions: inserting $it into sessionDao")
                 sessionDao.upsertSession(it)
             }
         }
--- app/src/main/java/io/github/drp08/studypal/domain/scheduler/RandomiseScheduler.kt
+++ app/src/main/java/io/github/drp08/studypal/domain/scheduler/RandomiseScheduler.kt
@@ -1,11 +1,13 @@
 package io.github.drp08.studypal.domain.scheduler
 
+import android.util.Log
 import io.github.drp08.studypal.domain.entities.SessionEntity
 import io.github.drp08.studypal.domain.entities.SubjectEntity
 import io.github.drp08.studypal.domain.entities.TopicEntity
 import io.github.drp08.studypal.domain.models.Session
 import io.github.drp08.studypal.domain.models.User
 import io.github.drp08.studypal.domain.scheduler.Scheduler.Companion.HOUR_IN_MILLIS
+import io.github.drp08.studypal.utils.formatTime
 import io.github.drp08.studypal.utils.toEpochMilliSecond
 import java.time.LocalDate
 import java.time.ZoneId
@@ -15,14 +17,19 @@ import kotlin.random.Random
 class RandomiseScheduler @Inject constructor() : Scheduler {
     companion object {
         private const val SESSION_DURATION = 1 * HOUR_IN_MILLIS
+        private const val TAG = "RandomiseScheduler"
     }
-
+    
     override fun schedule(
         subjects: List<SubjectEntity>,
         topics: List<TopicEntity>,
         fixedSessions: List<SessionEntity>, // TODO this is not used yet
         user: User
     ): List<SessionEntity> {
+        Log.d(
+            TAG,
+            "schedule() called with: subjects = $subjects, topics = $topics, fixedSessions = $fixedSessions, user = $user"
+        )
         val sessions = mutableListOf<SessionEntity>()
 
         val startOfDay =
@@ -43,6 +50,7 @@ class RandomiseScheduler @Inject constructor() : Scheduler {
                 }
                 .also { if (it.isEmpty()) return emptyList() }
                 .random()
+            Log.d(TAG, "schedule: chosen random subject $subject")
 
             val subjectTopics = topics.filter { it.subject == subject.name }
             if (subjectTopics.isEmpty()) // Make sure the subject have topics.
@@ -50,6 +58,7 @@ class RandomiseScheduler @Inject constructor() : Scheduler {
 
             // choose a random topic
             val topic = subjectTopics.random()
+            Log.d(TAG, "schedule: chosen random topic $topic")
 
             // epoch time from the start of the day to the start of the working hour
             val startTime = startOfDay + user.startWorkingHours
@@ -63,6 +72,7 @@ class RandomiseScheduler @Inject constructor() : Scheduler {
                     continue@outer
             } while (scheduledHours.any { time in it || (time + SESSION_DURATION) in it })
 
+            Log.d(TAG, "schedule: Chosen random time ${formatTime(time, "dd/MM/yyyy HH:mm:ss")}")
             scheduledHours.add(time..(time + SESSION_DURATION))
 
             // create a new session
@@ -74,11 +84,11 @@ class RandomiseScheduler @Inject constructor() : Scheduler {
                 )
 
             sessions.add(session)
+            Log.d(TAG, "schedule: Added $session to list of sessions")
             subjectTotalTime[subject.name] =
                 (subjectTotalTime[subject.name] ?: 0) + SESSION_DURATION
             studyHoursOfDay += SESSION_DURATION
         }
         return sessions
-            .sortedBy { it.startTime }
     }
 }
